09ade11e6f1ad6ba9b0eb8b430941386a4742752
Boidaz <145196575+Boidaz@users.noreply.github.com>
2024-06-22 13:46:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,31 +14,31 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.ytname = "YT: Boidaz" //ur yt chanel name
+global.socialm = "GitHub: Boidaz" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
+global.botname = 'FearMeBot V11' //ur bot name
+global.ownernumber = '2348160118233' //ur owner number
+global.ownername = '@FearMeBot' //ur owner name
+global.websitex = "https://youtu.com/@boidaz"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "FearMeBot."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "FearMeBot"
+global.author = "FearMeBot\n\n+2348160118233"
+global.creator = "2348160118233@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["8160118233"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '9' //set autoblock country code
+global.antiforeignnumber = '9' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
